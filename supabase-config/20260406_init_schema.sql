1bfa1a01aadb65cdecf5220551b6f3abb701c9a2
Xuan Vi <viazael69@gmail.com>
2026-04-19 09:29:32 +0700

    chore: Cập nhật và bổ sung trong supabase-config
    - Thêm tiền tố vào các file sql theo định dạng YYYYMMDD(NămThángNgày) để theo dõi cập nhật của Database
    - Đổi tên file init_schema.sql thành 20260406_init_schema.sql
    - Thêm file 20260418_add_constraint_policy_cart_items.sql để thêm Policy để bảo vệ dữ liệu giỏ hàng

--- supabase-config/20260406_init_schema.sql
+++ supabase-config/20260406_init_schema.sql
@@ -120,20 +120,15 @@ CREATE TRIGGER on_auth_user_created
   FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
 
 -- 5. KHAI BÁO POLICY CHO RLS TRÊN SUPABASE
-ALTER TABLE categories ENABLE ROW LEVEL SECURITY;
 
--- Tạo Policy cho phép mọi người (public) được Đọc (SELECT)
+-- 5.1 Tạo Policy cho phép mọi người (public) được Đọc (SELECT)
 CREATE POLICY "Cho phép tat ca moi nguoi xem danh muc"
 ON categories
 FOR SELECT
 TO public
-USING (true); 
--- USING(TRUE) cho phép ai cũng được xem danh mục món ăn
+USING (true); -- USING(TRUE) cho phép ai cũng được xem danh mục món ăn
 
--- 1. Bật RLS cho bảng carts
-ALTER TABLE carts ENABLE ROW LEVEL SECURITY;
-
--- 2. Tạo Policy cho phép người dùng đã đăng nhập thao tác (Xem, Thêm, Sửa, Xóa) trên giỏ hàng của họ
+-- 5.2 Tạo Policy cho phép người dùng đã đăng nhập thao tác (Xem, Thêm, Sửa, Xóa) trên giỏ hàng của họ
 CREATE POLICY "Nguoi dung quan ly gio hang cua chinh minh"
 ON carts
 FOR ALL -- Bao gồm SELECT, INSERT, UPDATE, DELETE
