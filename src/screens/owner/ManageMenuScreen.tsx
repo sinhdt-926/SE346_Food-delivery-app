a1ea71259b8c7cd13afa8760b9687b9a971510a4
kaydag <dangmaikhanhkn@gmail.com>
2026-06-01 01:47:28 +0700

    fix(menuscreen): chỉnh ui nút sort cho menuscreen

--- src/screens/owner/ManageMenuScreen.tsx
+++ src/screens/owner/ManageMenuScreen.tsx
@@ -285,10 +285,8 @@ export default function ManagerMenuScreen() {
             style={styles.overlay}
             onPress={() => setShowSortModal(false)}
           />
-
           <View style={styles.bottomSheet}>
             <View style={styles.dragBar} />
-
             <TouchableOpacity
               style={styles.categoryItem}
               onPress={() => {
@@ -542,16 +540,16 @@ const styles = StyleSheet.create({
   },
   sortButton: {
     marginRight: 20,
+    marginBottom: 15,
     width: 110,
     height: 40,
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
     backgroundColor: "#FFF",
     borderWidth: 1,
     borderColor: "#E5E5E5",
     borderRadius: 28,
-    paddingHorizontal: 15,
+    paddingHorizontal: 20,
   },
   sortText: {
     flex: 1,
